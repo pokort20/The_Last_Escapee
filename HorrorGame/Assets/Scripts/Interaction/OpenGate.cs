2f5969675b02df174a591e6913652c5ea45ed8ce
pokort20 <56198948+pokort20@users.noreply.github.com>
2021-11-17 22:45:05 +0100

    Added gate opener, opening gate scripts

--- HorrorGame/Assets/Scripts/Interaction/OpenGate.cs
+++ HorrorGame/Assets/Scripts/Interaction/OpenGate.cs
@@ -5,9 +5,11 @@ public class OpenGate : Interactable
     public GameObject Gate;
     public bool isClosed;
 
-    float movementDuration = 2.0f;
+    float movementDuration = 3.0f;
     private Vector3 targetPos;
     private Vector3 startPos;
+    private float startRot;
+    private float targetRot;
     private float elapsedTime;
     private bool isMoving;
     private float percentageMoved;
@@ -26,15 +28,18 @@ public class OpenGate : Interactable
     private void setMovementVariables()
     {
         startPos = Gate.transform.position;
+        startRot = transform.rotation.z;
         isMoving = false;
         elapsedTime = 0.0f;
         if (isClosed)
         {
             targetPos = new Vector3(startPos.x, startPos.y + 1.9f, startPos.z);
+            targetRot = startRot + 360.0f;
         }
         else
         {
             targetPos = new Vector3(startPos.x, startPos.y - 1.9f, startPos.z);
+            targetRot = startRot - 360.0f;
         }
     }
     void Update()
@@ -44,6 +49,10 @@ public class OpenGate : Interactable
             elapsedTime += Time.deltaTime;
             percentageMoved = elapsedTime / movementDuration;
             Gate.transform.position = Vector3.Lerp(startPos, targetPos, Mathf.SmoothStep(0, 1, percentageMoved));
+            Debug.Log("Rotation: " + Mathf.SmoothStep(startRot, targetRot, percentageMoved));
+            transform.RotateAround(transform.position, transform.forward, Mathf.SmoothStep(0, 3, percentageMoved));
+            //transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(startRot, targetRot, percentageMoved), Vector3.forward);
+            //transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Mathf.Lerp(startRot, targetRot, percentageMoved));
             Debug.Log("precentage finished: " + percentageMoved);
             if (percentageMoved >= 1.0f)
             {
