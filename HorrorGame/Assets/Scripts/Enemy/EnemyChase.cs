2e8ae8a2ff5a859e25078aa5471da2930b02d8ed
pokort20 <56198948+pokort20@users.noreply.github.com>
2022-01-11 18:22:51 +0100

    AI Update, patrol, chase, explore

--- HorrorGame/Assets/Scripts/Enemy/EnemyChase.cs
+++ HorrorGame/Assets/Scripts/Enemy/EnemyChase.cs
@@ -8,44 +8,168 @@ public class EnemyChase : MonoBehaviour
 
     public NavMeshAgent agent;
     public Transform player;
+    public GameObject patrolPoints;
+    public float enemyFOV;
+    public float agentSpeed;
+
+    private enum enemyStates : int
+    {
+        PATROL = 0,
+        CHASING = 1,
+        EXPLORE_AREA = 2
+    }
+
+    private enemyStates enemyState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyState = enemyStates.PATROL;
+        agent.speed = agentSpeed;
     }
 
     void FixedUpdate()
     {
+        switch (enemyState)
+        {
+            case enemyStates.PATROL:
+                //Debug.Log("PATROL");
+                patrol();
+                break;
+            case enemyStates.CHASING:
+                //Debug.Log("CHASE");
+                chase();
+                break;
+            case enemyStates.EXPLORE_AREA:
+                //Debug.Log("EXPLORE_AREA");
+                explore();
+                break;
+            default:
+                //Debug.Log("Enemy state unknown!");
+                break;
+        }
+        //if(isPlayerVisible(agent.transform.position, player.position))
+        //{
+        //    agent.SetDestination(player.position);
+        //}
+    }
+    private void patrol()
+    {
         if(isPlayerVisible(agent.transform.position, player.position))
         {
+            Debug.Log("Player spotted, switching to CHASING!");
+            enemyState = enemyStates.CHASING;
+            agent.speed = agentSpeed * 2.0f;
+            //chase();
+            return;
+        }
+        if(reachedDestination())
+        {
+            agent.ResetPath();
+            Debug.Log("Enemy reached destination!");
+        }
+        if (!agent.hasPath /*&& !isPlayerVisible(agent.transform.position, player.position)*/)
+        {
+            //find new patrol point
+
+            Transform selectedPatrolPoint = patrolPoints.transform.GetChild(Random.Range(0, patrolPoints.transform.childCount));
+            Debug.Log("Selected patrol point: " + selectedPatrolPoint.name);
+            agent.SetDestination(selectedPatrolPoint.position);
+            //foreach(Transform point in patrolPoints.transform)
+            //{
+            //    Debug.Log("Patrol point: " + point.name);
+            //}
+            //Debug.Log("Enemy is not moving");
+        }
+    }
+    private void chase()
+    {
+        if (isPlayerVisible(agent.transform.position, player.position))
+        {
+            //Still can see player
             agent.SetDestination(player.position);
         }
-
+        else if (agent.hasPath)
+        {
+            //Can not see, but still going to the last pos where player was seen
+            if (reachedDestination())
+            {
+                agent.ResetPath();
+                Debug.Log("Enemy reached chasing destination!");
+            }
+        }
+        else
+        {
+            //Can't see player nor has destination to go to
+            enemyState = enemyStates.EXPLORE_AREA;
+            agent.speed = agentSpeed*1.5f;
+            Debug.Log("Switching to EXPLORE_AREA!");
+        }
+    }
+    private void explore()
+    {
+        if (isPlayerVisible(agent.transform.position, player.position))
+        {
+            Debug.Log("Player spotted while exploring area, switching to CHASING!");
+            enemyState = enemyStates.CHASING;
+            agent.speed = agentSpeed * 2.0f;
+            chase();
+            return;
+        }
+        if (!agent.hasPath /*&& !isPlayerVisible(agent.transform.position, player.position)*/)
+        {
+            //find closest point and explore
+            float bestDistance = -1.0f;
+            float currentDistance;
+            Transform toBeExplored = null;
+            foreach(Transform patrolPoint in patrolPoints.transform)
+            {
+                currentDistance = Vector3.Distance(agent.transform.position, patrolPoint.position);
+                if (currentDistance < bestDistance || bestDistance == -1.0f)
+                {
+                    toBeExplored = patrolPoint;
+                    bestDistance = currentDistance;
+                }
+            }
+            Debug.Log("Selected point to explore: " + toBeExplored.name);
+            agent.SetDestination(toBeExplored.position);
+        }
+        if (reachedDestination())
+        {
+            Debug.Log("Explored area, returning to PATROL!");
+            agent.ResetPath();
+            enemyState = enemyStates.PATROL;
+            agent.speed = agentSpeed;
+            return;
+        }
+    }
+    private bool reachedDestination()
+    {
+        if(Vector3.Distance(agent.transform.position, agent.destination) < 1.05f * agent.stoppingDistance)
+        {
+            return true;
+        }
+        return false;
     }
     private bool isPlayerVisible(Vector3 agentPos, Vector3 playerPos)
     {
         RaycastHit raycastHit;
         float visibilityDistance = 10.0f;
+        
         Vector3 eyePos = agentPos;
         eyePos.y += 1.2f;
+        Vector3 dirToPlayer = playerPos - eyePos;
+        float angle = Vector3.Angle(dirToPlayer, agent.transform.forward);
+        //Debug.Log("angle: " + angle + ", DirToPlayer: " + dirToPlayer + ", agent fw: " + agent.transform.forward);
         if (Physics.Raycast(eyePos, playerPos - eyePos, out raycastHit, visibilityDistance))
         {
+            //Debug.Log("IsPlayerVisible RaycastHit: " + raycastHit.collider.gameObject.name);
             //Debug.Log("AgentPos: " + eyePos + ", PlayerPos: " + playerPos);
             //Debug.Log("Distance: " + Vector3.Distance(eyePos, raycastHit.collider.gameObject.transform.position));
-            if (raycastHit.collider.gameObject.layer == 10)
+            if (raycastHit.collider.gameObject.layer == 10 && angle < enemyFOV/2)
             {
-                Debug.Log("Enemy sees player!");
+                //Debug.Log("Enemy sees player!");
+                //enemyState = enemyStates.CHASING;
                 return true;
-                /*
-                interactingObject = raycastHit.collider.gameObject;
-                //Debug.Log("Hit interactable object!" + raycastHit.collider.gameObject.ToString());
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    interactingObject.GetComponent<Interactable>().Interact();
-                    //Debug.Log("Interact!");
-
-                }
-                */
             }
         }
         return false;
