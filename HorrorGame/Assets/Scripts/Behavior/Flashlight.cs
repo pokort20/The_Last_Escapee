9c1e32f20231177c4e8d9dda34d0d260db189bc1
pokort20 <56198948+pokort20@users.noreply.github.com>
2022-03-05 19:54:59 +0100

    HDRP, changed all textures and materials

--- HorrorGame/Assets/Scripts/Behavior/Flashlight.cs
+++ HorrorGame/Assets/Scripts/Behavior/Flashlight.cs
@@ -34,7 +34,7 @@ public class Flashlight : MonoBehaviour
                 return;
             }
             GameManager.instance.batteryLevel -= 1.0f * Time.deltaTime;
-            updateLightIntensity();
+            //updateLightIntensity();
         }
         //Debug.Log("Battery level: " + GameManager.instance.batteryLevel);
     }
